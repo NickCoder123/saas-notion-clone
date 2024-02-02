c2574801486286710df4dfa046a0be4810269a39
Nikunj Mehra <nikunjmehra935@gmail.com>
2024-02-02 18:32:43 +0530

    Finished landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import db from "@/lib/supabase/db";
+
+import ThemeProvider from "@/lib/providers/next-theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +16,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log(db);
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
